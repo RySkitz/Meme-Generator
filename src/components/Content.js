82c8defe8dcf0873103e78954430bc4f633ec34f
ryze <victorfabellomain@gmail.com>
2023-08-12 16:41:18 +0800

    text (self made)

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,52 @@
 import React from "react"
+import {useState} from "react"
+import memesData from "./memesData"
 
 export default function Content(){
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '', 
+        randomImage: 'https://i.imgflip.com/1ihzfe.jpg'
+    })
+
+    const [allMemeImages, setAllMemesImages] = useState(memesData)
+
+    const [memeImage, setMemeImage] = useState('')
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes 
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = (memesArray[randomNumber].url)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
+    const [aboveText, setAboveText] = useState("")
+    const [belowText, setBelowText] = useState("")
+
+    function changeAboveText(event){
+        setAboveText(event.target.value)
+    }
+    function changeBelowText(event){
+        setBelowText(event.target.value)
+    }
+
     return(
         <> 
             <div className="Position">
                 <div className="Container">
                     <div className="Input">
-                        <input className="Input-Top" placeholder="Text Above"/>
-                        <input className="Input-Bottom" placeholder="Text Below"/>
-                        <button className="Generate-Button">Get a new meme image</button>                        
+                        <input className="Input-Top" placeholder="Text Above" onChange={changeAboveText}/>
+                        <input className="Input-Bottom" placeholder="Text Below" onChange={changeBelowText}/>
+                        <button className="Generate-Button" onClick={getMemeImage}>Get a new meme image</button>                        
                     </div>
                     <div className="Image">
-                        <img src="/images/memeimg.png" alt="meme"/>
-                    </div>
+                        <img src={meme.randomImage} alt="meme" className="Image--Meme"/>
+                        <p className="Text-Above">{aboveText}</p>
+                        <p className="Text-Below">{belowText}</p>
+                    </div> 
                 </div>
             </div>
         </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Navbar(){
     return (
         <>
-            <nav className="Navbar">
-                <div className="Navbar--Logo">
-                    <img src="images/TrollFace.png"/>
+                <nav className="Navbar">
+                    <div className="Navbar--Logo">
+                    <img src="images/TrollFace.png" alt="Logo" draggable="false"/>
                     <h1 className="Navbar--Logo__Text">Meme Generator</h1>
                 </div>
                 <h2 className="Navbar--Project">React Course - Project 3</h2>
